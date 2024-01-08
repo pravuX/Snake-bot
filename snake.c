8eb0dfca926b24eb0bb366d711a4a6fb65892d9a
Prabesh Subedi <prabesh.sb@gmail.com>
2024-01-08 16:20:17 +0545

    Clean up unnecessary code

--- snake.c
+++ snake.c
@@ -5,14 +5,14 @@
 #define WINDOW_X 500
 #define WINDOW_Y 50
 
-#define GRID_SIZE 14
+#define GRID_SIZE 28
 #define TREE_SIZE (GRID_SIZE / 2)
 #define V (TREE_SIZE * TREE_SIZE)
 #define V_M (GRID_SIZE * GRID_SIZE)
 #define MAX_SNAKE_SIZE (GRID_SIZE * GRID_SIZE)
 #define GRID_DIM 800
 
-#define DELAY 80
+#define DELAY 40
 
 typedef enum {
   UP,    // 0
@@ -106,6 +106,7 @@ void delete_snake() {
 
 void render_grid(SDL_Renderer *renderer, int x, int y) {
   SDL_SetRenderDrawColor(renderer, 0x28, 0x28, 0x28, 255);
+  // set 1 for rendering grid
 #if 0
 
   int cell_size = GRID_DIM / GRID_SIZE;
@@ -118,7 +119,6 @@ void render_grid(SDL_Renderer *renderer, int x, int y) {
     for (int j = 0; j < GRID_SIZE; j++) {
       cell.x = x + i * cell_size;
       cell.y = y + j * cell_size;
-      // SDL_RenderFillRect(renderer, &cell); // no fill
       SDL_RenderDrawRect(renderer, &cell); // border only
     }
   }
@@ -147,7 +147,6 @@ void render_fruit(SDL_Renderer *renderer, int x, int y) {
 
   SDL_RenderFillRect(renderer, &fruit_seg);
 }
-
 
 void gen_fruit() {
   int x, y;
@@ -303,54 +302,6 @@ void render_snake(SDL_Renderer *renderer, int x, int y) {
   }
 }
 
-void flash_snake(SDL_Renderer *renderer, int x, int y) {
-
-  int seg_size = GRID_DIM / GRID_SIZE;
-
-  SDL_Rect seg;
-  seg.w = seg_size - 2;
-  seg.h = seg_size - 2;
-
-  SDL_Rect seg_out;
-  seg_out.w = seg_size;
-  seg_out.h = seg_size;
-
-  int bright = 255;
-  int b_dir = 0;
-
-  int r = rand() % 255;
-  int g = rand() % 255;
-  int b = rand() % 255;
-
-  for (int i = 0; i < snake_size; i++) {
-    int snake_index = mod(snake_begin_index + i, MAX_SNAKE_SIZE);
-
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, bright, 255);
-    seg_out.x = x + snake[snake_index].x * seg_size;
-    seg_out.y = y + snake[snake_index].y * seg_size;
-    SDL_RenderFillRect(renderer, &seg_out);
-
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-    seg.x = x + snake[snake_index].x * seg_size + 1;
-    seg.y = y + snake[snake_index].y * seg_size + 1;
-
-    SDL_RenderFillRect(renderer, &seg);
-
-    if (b_dir == 0) {
-      bright -= 5;
-      if (bright < 150) {
-        b_dir = 1;
-      }
-    }
-    if (b_dir == 1) {
-      bright += 5;
-      if (bright > 250) {
-        b_dir = 0;
-      }
-    }
-  }
-}
-
 void render_score(SDL_Renderer *renderer, int x, int y) {
   SDL_SetRenderDrawColor(renderer, 0x00, 0x9f, 0xf9, 255);
 
@@ -407,6 +358,7 @@ void render_high_score(SDL_Renderer *renderer, int x, int y) {
 
 // Spanning Tree adjacency matrix
 bool spanning_tree[V][V] = {false};
+// To represent the presence of walls between tiles
 bool maze[V_M][V_M] = {false};
 
 void get_adjacent_nodes(int x, int y, Point *adjacent_nodes,
@@ -502,6 +454,7 @@ void gen_tree() {
     spanning_tree[a][b] = true;
     spanning_tree[b][a] = true;
     // WALLS
+    // these points are scaled and tranlated to map onto the grid
     int ax = current.x * 2;
     int ay = current.y * 2;
     int bx = next.x * 2;
@@ -565,7 +518,7 @@ void gen_tree() {
   // border of the grid is also a part of the maze
   int x_last = GRID_SIZE - 1;
   int y_last = x_last * GRID_SIZE;
-  // wall between topmost and bottom most tiles
+  // wall between topmost and bottommost tiles
   for (int i = 0; i <= x_last; i++) {
     maze[i][i + y_last] = true;
     maze[i + y_last][i] = true;
@@ -604,7 +557,7 @@ void render_tree(SDL_Renderer *renderer, int x, int y) {
     SDL_Rect temp = nodes[i];
     temp.x = temp.x - node_size / 8;
     temp.y = temp.y - node_size / 8;
-    SDL_RenderFillRect(renderer, &temp); // no fill
+    SDL_RenderFillRect(renderer, &temp);
   }
 
   // draw lines
@@ -716,6 +669,7 @@ void traverse_maze() {
   int b_r = try_r.x + GRID_SIZE * try_r.y;
   int b_f = try_f.x + GRID_SIZE * try_f.y;
   int b_l = try_l.x + GRID_SIZE * try_l.y;
+  // the right hand maze traversing algorithm
   if (maze[a][b_r] == false) {
     // no wall between head and right tile
     // turn right skip the rest
@@ -760,8 +714,6 @@ int main() {
   bool ai = false;
   bool show_tree = true;
   SDL_Event event;
-
-  int flash_dur = 0;
 
   while (!quit) {
     while (SDL_PollEvent(&event)) {
@@ -819,16 +771,6 @@ int main() {
     if (!show_tree) {
       render_tree(renderer, grid_x, grid_y);
     }
-
-    if (fruit.score % 10 == 0 && fruit.score != 0) {
-      flash_dur = 10;
-    }
-
-    if (flash_dur > 0) {
-      flash_snake(renderer, grid_x, grid_y);
-      flash_dur--;
-    }
-
     render_score(renderer, -25, grid_y + GRID_DIM);
     render_high_score(renderer, GRID_DIM - 3 * grid_x + 25, grid_y + GRID_DIM);
 
